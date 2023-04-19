8fb7c1178bdd10815f63f74332360dc6ab4809bc
MalachiRutter <malachicoberley@gmail.com>
2023-04-19 13:16:24 -0700

    part 2 of 2 of tree

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -12,7 +12,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question":question})
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}")
